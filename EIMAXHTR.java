8aedabbbb916df739c962b4a1e9fd4f7a51e3a3b
thucvanminh <thucnichan2929vmt@gmail.com>
2024-11-23 13:21:43 +0700

    Quick

---
+++ EIMAXHTR.java
@@ -0,0 +1,75 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EIMAXHTR {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int nVertices = sc.nextInt();
+        int nEdges = sc.nextInt();
+
+    }
+
+    private Vertex[] readGraph(int nVertices, int nEdges) {
+        Vertex[] result = new Vertex[nVertices];
+        for (int i = 0; i < nVertices; i++) {
+            result[i] = new Vertex(i);
+        }
+        for (int i = 0; i < nEdges; i++) {
+            int first = sc.nextInt();
+            int second = sc.nextInt();
+
+            result[first].adjancyList.add(result[second]);
+            result[second].adjancyList.add(result[first]);
+
+        }
+
+        return result;
+    }
+
+    private static class Vertex {
+        private int id;
+        private ArrayList<Vertex> adjancyList = new ArrayList<>();
+        private boolean visited;
+
+        public Vertex(int id) {
+            this.id = id;
+            visited = false;
+        }
+
+        public int getId() {
+            return id;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public ArrayList<Vertex> getAdjancyList() {
+            return adjancyList;
+        }
+
+        public void setAdjancyList(ArrayList<Vertex> adjancyList) {
+            this.adjancyList = adjancyList;
+        }
+    }
+
+    private int dfs(Vertex ver) {
+        if (ver == null) {
+            return 0;
+        }
+
+        int maxHeight = 0;
+        ver.visited = true;
+
+        for (int i = 0; i < ver.adjancyList.size(); i++) {
+            if (ver.adjancyList.get(i).visited == false) {
+                int childHeight = dfs(ver.adjancyList.get(i));
+                maxHeight = Math.max(maxHeight, childHeight);
+            }
+        }
+        return maxHeight + 1;
+
+    }
+}
