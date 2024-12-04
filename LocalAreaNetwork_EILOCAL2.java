d78b8fbc885d96988dd87849ad79408cefbaf789
thucvanminh <thucnichan2929vmt@gmail.com>
2024-12-04 10:22:48 +0700

    Quick

--- LocalAreaNetwork_EILOCAL2.java
+++ LocalAreaNetwork_EILOCAL2.java
@@ -8,35 +8,58 @@ public class LocalAreaNetwork_EILOCAL2 {
 	}
 
 	static class Vertex {
+		public boolean visited;
+		public int distance;
 		public int id;
-		public List<Vertex> adjecentVertices = new ArrayList<>();
+		public List<Edge> adjecentEdges = new ArrayList<>();
 
 		public Vertex(int id) {
 			this.id = id;
+			this.distance = 0;
+		}
+
+		public void addAdjacentEdges(Vertex v, int weight) {
+			
+			adjecentEdges.add(new Edge(v, weight));
+		}
+	}
+
+	static class Edge {
+		public int weight;
+		public Vertex endpoint;
+
+		public Edge(Vertex endpoint, int weight) {
+			this.weight = weight;
+			this.endpoint = endpoint;
+		}
+	}
+
+	static void dfs(Vertex v) {
+		v.visited = true;
+		for (int i = 0; i < v.adjecentEdges.size(); i++) {
+
 		}
 	}
 
 	static Vertex[] readGraph() {
 		int nVertices = sc.nextInt();
+		int nEdges = nVertices - 1;
 		Vertex[] vertices = new Vertex[nVertices];
 		for (int i = 0; i < nVertices; i++) {
 			vertices[i] = new Vertex(i);
 		}
 		int m = sc.nextInt();
 		for (int i = 0; i < m; i++) {
-			int a = sc.nextInt();
-			int b = sc.nextInt();
-			vertices[a].adjecentVertices.add(vertices[b]);
-			vertices[b].adjecentVertices.add(vertices[a]);
+			int from = sc.nextInt();
+			int to = sc.nextInt();
+			int weight = sc.nextInt();
+
+			vertices[from].addAdjacentEdges(vertices[to], weight);
+			vertices[to].addAdjacentEdges(vertices[from], weight);
+
 		}
 
-		for (Vertex each : vertices) {
-			Collections.sort(each.adjecentVertices, new Comparator<Vertex>() {
-				public int compare(Vertex v1, Vertex v2) {
-					return Integer.compare(v1.id, v2.id);
-				}
-			});
-		}
+		
 		return vertices;
 	}
 }
